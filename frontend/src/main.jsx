b0e988bc3c27b84c4915f8e0a34157cec92ebf13
Rajvardhan G <rajvardhan.ganji75@gmail.com>
2024-10-10 23:39:23 +0530

    frontend page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+// import './index.css'
+import './main.css'
+import Navbar from './components/Navbar.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div>
+    <Navbar />
+    {/* <App /> */}
+  </div>,
 )
