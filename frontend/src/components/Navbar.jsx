9d5136a274132b734d4a0ad8c08f323d6c7bd08c
veereshk20 <pandiriveereshkumar@gmail.com>
2025-01-03 19:28:27 +0530

    added routes to different pages

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,28 +1,56 @@
 import React from "react";
 import "./Navbar.css";
 import logo from "../assets/logo.webp";
+import {BrowserRouter, Link, Routes, Route} from "react-router-dom";
+import AboutUs from "./AboutUs";
+import SlotBooking from "./SlotBooking";
+import Profile from "./Profile";
+import Home from "./Home";
 
 function Navbar(){
     return(
-        <nav className="navbar">
-            <div className="logo">
-                <div className="logoImg">
-                    <img src={logo}/>
+        <BrowserRouter>
+            <nav className="navbar">
+                <div className="logo">
+                    <div className="logoImg">
+                        <img src={logo} href="/"/>
+                    </div>
+                    <Link to="/" className="logoText">
+                        IIITDM<br/>Kancheepuram
+                    </Link>
                 </div>
-                <a className="logoText">IIITDM<br/>Kancheepuram</a>
-            </div>
-            
-            
-            <ul className="navList">
-                <li><a href="/aboutUs">About Us</a></li>
-                <li><a href="/slotbooking">Slot Booking</a></li>
-                <li><a href="/profile">Profile</a></li>
-                <li id="auth-btn">
-                <a href="/auth/google/" className="signIn">Sign In</a>
-                </li>
-            </ul>
-            {/* <div className="bx bx-menu" id="menu-icon"></div> */}
-        </nav>
+                
+                <ul className="navList">
+                    <li>
+                        <Link to="/aboutus">
+                            About Us
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/slotbooking">
+                            Slot Booking
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/profile">
+                            Profile
+                        </Link>
+                    </li>
+                    <li id="auth-btn">
+                        <Link to="/auth/google/" className="signIn">
+                            Sign In
+                        </Link>
+                    </li>
+                </ul>
+                {/* <div className="bx bx-menu" id="menu-icon"></div> */}
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/aboutUs" element={<AboutUs />}></Route>
+                <Route path="/slotbooking" element={<SlotBooking />}></Route>
+                <Route path="/profile" element={<Profile />}></Route>
+            </Routes>
+        </BrowserRouter>        
     )
 }
 
---
+++ frontend/src/components/SlotBooking.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SlotBooking(){
+    return(
+        <div>
+            <h1>Slot Booking page</h1>
+        </div>
+    )    
+}
+
+export default SlotBooking;
